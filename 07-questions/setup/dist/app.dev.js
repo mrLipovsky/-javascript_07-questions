44975e6685812679c605f04293254ffbfbc41a35
mr_lipovsky <90620664+mrLipovsky@users.noreply.github.com>
2022-11-27 12:31:41 +0100

    Add files via upload

---
+++ 07-questions/setup/dist/app.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//using selectors inside the element
+var questions = document.querySelectorAll(".question");
+console.log(questions);
+questions.forEach(function (question) {
+  // console.log(question);
+  var btn = question.querySelector(".question-btn"); // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+}); // traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElemen;
+//         question.classList.toggle('show-text');
+//     });
+// });
